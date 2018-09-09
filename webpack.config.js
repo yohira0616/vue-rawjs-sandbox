91be8d7193739431d6e292a6e381c659a1960342
yukito ohira <yukito.sys.so@gmail.com>
2018-09-09 10:53:27 +0900

    webpack configure

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    devtool: 'source-map',
+    entry: ['./app.js'],
+    output: {
+        filename: './output.js',
+        path: path.resolve(__dirname),
+    },
+    serve: {},
+};
