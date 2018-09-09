70fee0fdb9dc17a6edd68af3b09251b391096de9
yukito ohira <yukito.sys.so@gmail.com>
2018-09-09 12:04:57 +0900

    グローバル変数としてvueインスタンスを外だし

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 console.log('hello,world!!!')
 
 document.addEventListener('DOMContentLoaded', () => {
-  var app = new Vue({
+  // web consoleでappをいじくれるようにする
+  window.app = new Vue({
     el: '#app',
     data: {
       message: 'Hello,Vue.js'
     }
   })
-
 })
 
